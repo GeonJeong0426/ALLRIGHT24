a3651e76076c7ca7d0c43a032c54fe0132ad342c
PARKJAEHWI <2606pjh@naver.com>
2024-04-20 03:34:05 +0900

    상세페이지 구현

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { ObjectId } = require("mongodb");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -39,12 +40,21 @@ app.get("/quiz", async (req, res) => {
   res.sendFile(__dirname + "/public/quiz.html");
 });
 
+// 오운완 서버 코드
+
 app.get("/OWunWan", async (req, res) => {
+  //게시글목록
   let result = await db.collection("OWunWan").find().toArray();
-  res.render("OWunWan.ejs", { 오운완: result });
+  res.render("OWunWan.ejs", { result: result });
+});
+
+app.get("/OWunWanWrite", async (req, res) => {
+  //오운완 게시물 작성
+  res.render("OWunWanWrite.ejs");
 });
 
 app.post("/OWunWan_post", async (req, res) => {
+  //오운완 게시물 DB저장
   try {
     if (req.body.title === "") {
       res.send("제목을 입력해주세요");
@@ -55,5 +65,20 @@ app.post("/OWunWan_post", async (req, res) => {
     }
   } catch (error) {
     res.send("DB error!");
+  }
+});
+
+app.get("/OWunWan/detail/:id", async (req, res) => {
+  // 오운완 상세페이지
+  try {
+    let result = await db.collection("OWunWan").findOne({ _id: new ObjectId(req.params.id) });
+    if (result == null) {
+      res.status(400).send("존재하지 않는 URL 입니다.");
+    } else {
+      res.render("OWunWanDetail.ejs", { result: result });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("존재하지 않는 URL 입니다.");
   }
 });
