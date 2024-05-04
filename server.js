85f5bed99c4803a804ab28d3eacb1795f8c1f55f
ParkJaeHwi <2606pjh@naver.com>
2024-05-04 22:56:40 +0900

    자세지식인CRUD기능구현

--- server.js
+++ server.js
@@ -111,8 +111,71 @@ app.delete("/OWunWan_delete/:id", async (req, res) => {
   res.redirect("/OWunWan");
 });
 
+//자세 봐주세요 게시판
+
 app.get("/PostureQnA", async (req, res) => {
-  //게시글목록
+  //자세 봐주세요 게시글목록
   let result = await db.collection("PostureQnA").find().toArray();
   res.render("PostureQnA.ejs", { result: result });
+});
+
+app.get("/PostureQnAWrite", async (req, res) => {
+  //자세 봐주세요 게시물 작성
+  res.render("PostureQnAWrite.ejs");
+});
+
+app.post("/PostureQnA_post", async (req, res) => {
+  //자세 봐주세요 게시물 등록하기
+  try {
+    if (req.body.title === "") {
+      res.send("제목을 입력해주세요");
+    } else {
+      //제목이 입력되었을때만 저장
+      await db.collection("PostureQnA").insertOne({ title: req.body.title, content: req.body.content });
+      res.redirect("/PostureQnA");
+    }
+  } catch (error) {
+    res.send("DB error!");
+  }
+});
+
+app.get("/PostureQnA/detail/:id", async (req, res) => {
+  // 자세 봐주세요 상세페이지
+  try {
+    let result = await db.collection("OWunWan").findOne({ _id: new ObjectId(req.params.id) });
+    if (result == null) {
+      res.status(400).send("존재하지 않는 URL 입니다.");
+    } else {
+      res.render("PostureQnADetail.ejs", { result: result });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("존재하지 않는 URL 입니다.");
+  }
+});
+
+app.get("/PostureQnA_edit/:id", async (req, res) => {
+  //수정할 게시글 불러오기
+  let result = await db.collection("PostureQnA").findOne({ _id: new ObjectId(req.params.id) });
+  res.render("PostureQnA_edit.ejs", { result: result });
+});
+
+app.put("/PostureQnA_edit_post/:id", async (req, res) => {
+  // 수정완료 버튼 누르면
+  try {
+    await db
+      .collection("PostureQnA")
+      .updateOne({ _id: new ObjectId(req.params.id) }, { $set: { title: req.body.title, content: req.body.content } });
+
+    res.redirect("/PostureQnA");
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("DB오류");
+  }
+});
+
+app.delete("/PostureQnA_delete/:id", async (req, res) => {
+  //삭제하기
+  await db.collection("PostureQnA").deleteOne({ _id: new ObjectId(req.params.id) });
+  res.redirect("/PostureQnA");
 });
